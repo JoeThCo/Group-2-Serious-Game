de9a14b7859fd441e3505f0733eb03d8b17b4385
JoeThCo <josephthco@gmail.com>
2021-03-01 10:54:24 -0500

    working on matches

---
+++ Group 2 Serious Game/Assets/Scripts/GameMananger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMananger : MonoBehaviour
+{
+    public int BoardSize;
+    [Range(1.5f,2.5f)]
+    public float Offset;
+    [Space(15)]
+    public GameObject Tile;
+    public Transform TileParent;
+    [Space(15)]
+    public GameObject[,] TileBoard;
+
+    private void Start()
+    {
+        TileBoard = new GameObject[BoardSize, BoardSize];
+
+        SpawnStartPieces();
+    }
+
+    void SpawnStartPieces() 
+    {
+        for (int rows = 0; rows < BoardSize; rows++)
+        {
+            for (int cols = 0; cols < BoardSize; cols++)
+            {
+                GameObject tile = Instantiate(Tile, new Vector2(BoardSize/2 * Offset - rows * Offset, BoardSize / 2 * Offset - cols * Offset), Quaternion.identity, TileParent);
+                TileBoard[rows, cols] = tile;
+
+                if (rows % 2 == 0)
+                    tile.GetComponent<SpriteRenderer>().color = Color.black;
+
+            }
+        }
+    }
+
+}
---
+++ Group 2 Serious Game/Assets/Scripts/TileController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public GameObject SwitchTile;
+
+    private Vector3 offset = new Vector3(0,0,10);
+    private Vector2 startSpot;
+    private Camera Cam;
+
+    private GameMananger gm;
+    private MatchManager mm;
+
+    private void Start()
+    {
+        Cam = Camera.main;
+
+        gm = FindObjectOfType<GameMananger>();
+        mm = FindObjectOfType<MatchManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        //get og position to return to if not a valid spot
+        startSpot = transform.position;
+
+        //visual of seletecing the object
+    }
+
+    private void OnMouseDrag()
+    {
+        DragTileAround();
+    }
+
+    private void OnMouseUp()
+    {
+        SwitchTileLocation();
+    }
+
+    void DragTileAround() 
+    {
+        //get the current location
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
+
+        //apply offset
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+        //set pos
+        transform.position = curPosition;
+    }
+
+    void SwitchTileLocation() 
+    {
+        if (SwitchTile && new Vector2(Mathf.Round(startSpot.x), Mathf.Round(startSpot.y)) != new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y)))
+        {
+            //snap to nearest whole number
+            transform.position = new Vector3(Mathf.RoundToInt(SwitchTile.transform.position.x), Mathf.RoundToInt(SwitchTile.transform.position.y), 0);
+            SwitchTile.transform.position = startSpot;
+
+            //swap board values
+            mm.SwapPiecesOnBoard(gameObject, SwitchTile);
+        }
+        else
+        {
+            transform.position = startSpot;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tile")) 
+        {
+            SwitchTile = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tile"))
+        {
+            SwitchTile = null;
+        }
+    }
+}
