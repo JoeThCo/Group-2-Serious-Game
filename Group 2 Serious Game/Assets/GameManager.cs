622acdfdca77fe364afba0a23ed4876adcde6229
JoeThCo <josephthco@gmail.com>
2021-03-01 20:02:57 -0500

    have make board, move pieces, and swap pieces done

---
+++ Group 2 Serious Game/Assets/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int BoardSize;
+    public float Offset;
+
+    public GameObject[,] Board;
+
+    [Space(15)]
+    public GameObject Tile;
+    public Transform TileParent;
+
+    private void Start()
+    {
+        Board = new GameObject[BoardSize, BoardSize];
+
+        StartGrid();
+    }
+
+    // make board
+    void StartGrid()
+    {
+        for (int y = 0; y < BoardSize; y++)
+        {
+            for (int x = 0; x < BoardSize; x++)
+            {
+                GameObject tile = Instantiate(Tile, new Vector2(BoardSize / 2 - x, BoardSize / 2 - y) * Offset, Quaternion.identity, TileParent);
+                Board[x, y] = tile;
+
+                tile.GetComponent<Tile>().Cords = new Vector2Int(x, y);
+                tile.name = "(" + x + "," + y + ")";
+
+                if (x % 2 == 0)
+                {
+                    tile.GetComponent<SpriteRenderer>().color = Color.black;
+                    tile.GetComponent<Tile>().Value = 1;
+                }
+            }
+        }
+    }
+
+    public void ComboChecker()
+    {
+        int currentCombo = Board[0, 0].GetComponent<Tile>().Value;
+        int combo = 1;
+
+        void Reset(int x, int y)
+        {
+            if(combo >= 3)
+                Debug.Log("Combo x" + combo.ToString());
+
+            combo = 1;
+            currentCombo = Board[x, y].GetComponent<Tile>().Value;
+        }
+
+        for (int y = 0; y < BoardSize; y++)
+        {
+            for (int x = 0; x < BoardSize; x++)
+            {
+                Debug.Log("Current Combo:" + currentCombo + " | " + Board[x, y].GetComponent<Tile>().Value);
+
+                //if not the same value - ending the combo
+                if (Board[x, y].GetComponent<Tile>().Value != currentCombo)
+                {
+                    //reset the combo
+                    Reset(x, y);
+                }
+                else
+                {
+                    combo++;
+                }
+            }
+            Reset(BoardSize - 1, y);
+        }
+    }
+}
--- Group 2 Serious Game/Assets/Scripts/Tile.cs
+++ Group 2 Serious Game/Assets/Scripts/Tile.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileController : MonoBehaviour
+public class Tile : MonoBehaviour
 {
+    public int Value;
+    public Vector2Int Cords;
+    [Space(15)]
     public GameObject SwitchTile;
 
-    private Vector3 offset = new Vector3(0,0,10);
-    private Vector2 startSpot;
+    private Vector3 offset = new Vector3(0, 0, 10);
+    private Vector3 startSpot;
     private Camera Cam;
-
-    private GameMananger gm;
-    private MatchManager mm;
+    private GameManager gm;
 
     private void Start()
     {
         Cam = Camera.main;
-
-        gm = FindObjectOfType<GameMananger>();
-        mm = FindObjectOfType<MatchManager>();
+        gm = FindObjectOfType<GameManager>();
     }
 
+    //2 pick up piece
     private void OnMouseDown()
     {
         //get og position to return to if not a valid spot
-        startSpot = transform.position;
+        startSpot = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y),0);
 
         //visual of seletecing the object
     }
@@ -37,9 +37,13 @@ public class TileController : MonoBehaviour
     private void OnMouseUp()
     {
         SwitchTileLocation();
+
+        gm.ComboChecker();
+
+        SwitchTile = null;
     }
 
-    void DragTileAround() 
+    void DragTileAround()
     {
         //get the current location
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
@@ -51,7 +55,8 @@ public class TileController : MonoBehaviour
         transform.position = curPosition;
     }
 
-    void SwitchTileLocation() 
+    //3 swap tiles on move
+    void SwitchTileLocation()
     {
         if (SwitchTile && new Vector2(Mathf.Round(startSpot.x), Mathf.Round(startSpot.y)) != new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y)))
         {
@@ -60,7 +65,11 @@ public class TileController : MonoBehaviour
             SwitchTile.transform.position = startSpot;
 
             //swap board values
-            mm.SwapPiecesOnBoard(gameObject, SwitchTile);
+            //Debug.Log(gm.Board[Mathf.RoundToInt(SwitchTile.GetComponent<Tile>().Cords.x), Mathf.RoundToInt(SwitchTile.GetComponent<Tile>().Cords.y)].name);
+           //Debug.Log(gm.Board[Mathf.RoundToInt(transform.GetComponent<Tile>().Cords.x), Mathf.RoundToInt(transform.GetComponent<Tile>().Cords.y)].name);
+
+            gm.Board[Mathf.RoundToInt(SwitchTile.GetComponent<Tile>().Cords.x), Mathf.RoundToInt(SwitchTile.GetComponent<Tile>().Cords.y)] = gameObject;
+            gm.Board[Mathf.RoundToInt(transform.GetComponent<Tile>().Cords.x), Mathf.RoundToInt(transform.GetComponent<Tile>().Cords.y)] = SwitchTile;
         }
         else
         {
@@ -71,7 +80,7 @@ public class TileController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Tile")) 
+        if (collision.gameObject.CompareTag("Tile"))
         {
             SwitchTile = collision.gameObject;
         }
